8eeb75f3aac8eeba2cd6fa67612c1ce93c6e233f
AlejandroIPerezL <pantuyaamore@gmail.com>
2026-07-19 14:48:57 -0500

    Modificacion hasta busqueda binaria

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-drones=[]
-misiones=[]
-zona=[]
-rutas=[]
+drones = []
+misiones = []
+zona = []
+grafo_zonas = {}
+rutas = []
+
+def mostrar_codigos(lista, clave_codigo):
+    codigos = []
+    for elemento in lista:
+        codigos.append(elemento[clave_codigo])
+    print(codigos)
 
 def registrar_dron():
     codigo = input("Código del dron: ")
@@ -10,14 +17,14 @@ def registrar_dron():
     capacidad = float(input("Ingrese la capacidad del dron: "))
     bateria = float(input("Nivel de batería (%): "))
     estado = input("Ingrese el estado del dron Ejm: Disponible, En mision, En mantenimiento: ")
-    dron = {"codigo": codigo, "modelo": modelo, "velocidad": velocidad,"Capacidad": capacidad ,"bateria": bateria, "Estado": estado}
+    dron = {"codigo": codigo, "modelo": modelo, "velocidad": velocidad, "Capacidad": capacidad, "bateria": bateria, "Estado": estado}
     drones.append(dron)
     print("Dron registrado correctamente.")
 
 def mostrar_drones():
     print("Drones registrados")
     for i in drones:
-        print("Código:", i["codigo"], "| Modelo:", i["modelo"], "| Velocidad:", i["velocidad"],"| Capacidad", i["Capacidad"]  , "| Batería:", i["bateria"], "| Estado:", i["Estado"])
+        print("Código:", i["codigo"], "| Modelo:", i["modelo"], "| Velocidad:", i["velocidad"], "| Capacidad", i["Capacidad"], "| Batería:", i["bateria"], "| Estado:", i["Estado"])
 
 def eliminar_dron():
     codigo = input("Código del dron a eliminar: ")
@@ -30,15 +37,16 @@ def eliminar_dron():
         print("Dron eliminado.")
     else:
         print("Dron no encontrado.")
+
 def registrar_misiones():
     codigo = input("Ingrese el codigo de la mision: ")
-    zona = input("Ingrese la zona de la mision: ")
+    nombre_zona = input("Ingrese la zona de la mision: ")
     Tipo = input("Ingrese el tipo de emergencia: ")
-    prioridad = int(input("Ingrese el nivel de prioridad ejm: 1.- Maxima prioridad 2.- Alta prioridad 3.- Prioridad Media 3.- Prioridad Baja solo numeros: "))
+    prioridad = int(input("Ingrese el nivel de prioridad ejm: 1.- Maxima prioridad 2.- Alta prioridad solo numeros: "))
     personas = int(input("Ingrese el numero de personas afectadas: "))
     distancia = float(input("Ingrese la distacia a la zona: "))
     estado = input("Ingrese el estado de la mision ejmp pendiente, en curso, terminada: ")
-    mision = {"Codigo": codigo,"Zona":zona,"Tipo": Tipo, "Prioridad": prioridad, "Personas afectadas":personas, "Distancia": distancia, "Estado": estado}
+    mision = {"Codigo": codigo, "Zona": nombre_zona, "Tipo": Tipo, "Prioridad": prioridad, "Personas afectadas": personas, "Distancia": distancia, "Estado": estado}
     misiones.append(mision)
     print("Mision registradad exitosamente")
 
@@ -46,38 +54,333 @@ def mostrar_misiones():
     print("Misiones registradas")
     for i in misiones:
         print("Código:", i["Codigo"], "| Zona:", i["Zona"], "| Tipo:", i["Tipo"], "| Prioridad:", i["Prioridad"], "| Personas afectadas:", i["Personas afectadas"], "| Distancia:", i["Distancia"], "| Estado:", i["Estado"])
+
 def eliminar_mision():
-    codigo = input("Código del dron a eliminar: ")
+    codigo = input("Código de la misión a eliminar: ")
     posicion = -1
     for i in range(len(misiones)):
-        if misiones[i]["codigo"] == codigo:
+        if misiones[i]["Codigo"] == codigo:
             posicion = i
     if posicion != -1:
         misiones.pop(posicion)
-        print("Dron eliminado.")
+        print("Misión eliminada.")
     else:
+        print("Misión no encontrada.")
+
+def registrar_zona():
+    nombre = input("Ingrese el nombre de la zona: ")
+    ya_existe = 0
+    for z in zona:
+        if z == nombre:
+            ya_existe = 1
+    if ya_existe == 0:
+        zona.append(nombre)
+        grafo_zonas[nombre] = {}
+        print("Zona registrada exitosamente.")
+    else:
+        print("La zona ya existe.")
+
+def mostrar_zonas():
+    print("Zonas registradas:", zona)
+
+def eliminar_zona():
+    nombre = input("Ingrese el nombre de la zona a eliminar: ")
+    posicion = -1
+    for i in range(len(zona)):
+        if zona[i] == nombre:
+            posicion = i
+    if posicion != -1:
+        zona.pop(posicion)
+        if nombre in grafo_zonas:
+            del grafo_zonas[nombre]
+        print("Zona eliminada.")
+    else:
+        print("Zona no encontrada.")
+
+def registrar_ruta():
+    origen = input("Ingrese la zona de origen: ")
+    destino = input("Ingrese la zona de destino: ")
+    distancia = float(input("Ingrese la distancia en km: "))
+    existe_origen = 0
+    existe_destino = 0
+    for z in zona:
+        if z == origen: existe_origen = 1
+        if z == destino: existe_destino = 1
+    if existe_origen == 1 and existe_destino == 1:
+        grafo_zonas[origen][destino] = distancia
+        grafo_zonas[destino][origen] = distancia
+        print("Ruta registrada exitosamente.")
+    else:
+        print("Error: Una o ambas zonas no existen.")
+
+def mostrar_rutas():
+    for z in grafo_zonas:
+        print(z, "->", grafo_zonas[z])
+
+def eliminar_ruta():
+    origen = input("Ingrese la zona de origen de la ruta a eliminar: ")
+    destino = input("Ingrese la zona de destino de la ruta a eliminar: ")
+    if origen in grafo_zonas and destino in grafo_zonas[origen]:
+        del grafo_zonas[origen][destino]
+        del grafo_zonas[destino][origen]
+        print("Ruta eliminada correctamente.")
+    else:
+        print("Ruta no encontrada.")
+
+def burbuja_prioridad():
+    n = len(misiones)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            print("Comparando...", misiones[j]["Codigo"], "con", misiones[j + 1]["Codigo"])
+            if misiones[j]["Prioridad"] > misiones[j + 1]["Prioridad"]:
+                print("Intercambiando...")
+                temp = misiones[j]
+                misiones[j] = misiones[j + 1]
+                misiones[j + 1] = temp
+        print("Lista actual:")
+        mostrar_codigos(misiones, "Codigo")
+
+def insercion_bateria():
+    for i in range(1, len(drones)):
+        actual = drones[i]
+        j = i - 1
+        print("Insertando:", actual["codigo"])
+        while j >= 0 and drones[j]["bateria"] > actual["bateria"]:
+            print("Moviendo:", drones[j]["codigo"])
+            drones[j + 1] = drones[j]
+            j = j - 1
+        drones[j + 1] = actual
+        print("Lista actual:")
+        mostrar_codigos(drones, "codigo")
+
+def mergesort_velocidad(lista):
+    if len(lista) > 1:
+        medio = len(lista) // 2
+        izquierda = lista[:medio]
+        derecha = lista[medio:]
+        print("Dividiendo")
+        mostrar_codigos(lista, "codigo")
+        mergesort_velocidad(izquierda)
+        mergesort_velocidad(derecha)
+        i = j = k = 0
+        while i < len(izquierda) and j < len(derecha):
+            if izquierda[i]["velocidad"] < derecha[j]["velocidad"]:
+                lista[k] = izquierda[i]
+                i += 1
+            else:
+                lista[k] = derecha[j]
+                j += 1
+            k += 1
+        while i < len(izquierda):
+            lista[k] = izquierda[i]
+            i += 1
+            k += 1
+        while j < len(derecha):
+            lista[k] = derecha[j]
+            j += 1
+            k += 1
+        print("Mezclando")
+        print("Resultado parcial")
+        mostrar_codigos(lista, "codigo")
+
+def particion_distancia(lista, inicio, fin):
+    pivote = lista[fin]["Distancia"]
+    print("Pivote:", lista[fin]["Codigo"])
+    i = inicio - 1
+    for j in range(inicio, fin):
+        if lista[j]["Distancia"] <= pivote:
+            i += 1
+            temp = lista[i]
+            lista[i] = lista[j]
+            lista[j] = temp
+    temp = lista[i + 1]
+    lista[i + 1] = lista[fin]
+    lista[fin] = temp
+    print("Partición")
+    mostrar_codigos(lista, "Codigo")
+    return i + 1
+
+def quicksort_distancia(lista, inicio, fin):
+    if inicio < fin:
+        posicion = particion_distancia(lista, inicio, fin)
+        quicksort_distancia(lista, inicio, posicion - 1)
+        quicksort_distancia(lista, posicion + 1, fin)
+
+def busqueda_lineal_dron():
+    codigo = input("Código del dron a buscar: ")
+    comparaciones = 0
+    encontrado = 0
+    for d in drones:
+        comparaciones += 1
+        print("Elemento comparado:", d["codigo"])
+        if d["codigo"] == codigo:
+            encontrado = 1
+            print("Dron encontrado:", d)
+            break
+    print("Comparaciones realizadas:", comparaciones)
+    if encontrado == 0:
         print("Dron no encontrado.")
+
+def busqueda_lineal_mision():
+    codigo = input("Código de la misión a buscar: ")
+    comparaciones = 0
+    encontrado = 0
+    for m in misiones:
+        comparaciones += 1
+        print("Elemento comparado:", m["Codigo"])
+        if m["Codigo"] == codigo:
+            encontrado = 1
+            print("Misión encontrada:", m)
+            break
+    print("Comparaciones realizadas:", comparaciones)
+    if encontrado == 0:
+        print("Misión no encontrada.")
+
+def busqueda_binaria_dron():
+    n = len(drones)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if drones[j]["codigo"] > drones[j + 1]["codigo"]:
+                temp = drones[j]
+                drones[j] = drones[j + 1]
+                drones[j + 1] = temp
+    codigo = input("Código del dron a buscar (Binaria): ")
+    low = 0
+    high = len(drones) - 1
+    encontrado = 0
+    while low <= high:
+        mid = (low + high) // 2
+        print("low:", low, "| high:", high, "| mid:", mid, "| valor medio:", drones[mid]["codigo"])
+        if drones[mid]["codigo"] == codigo:
+            print("Dron encontrado:", drones[mid])
+            encontrado = 1
+            break
+        elif drones[mid]["codigo"] < codigo:
+            low = mid + 1
+        else:
+            high = mid - 1
+    if encontrado == 0:
+        print("Dron no encontrado.")
+
 def menuchi():
-    while 1:
-        print("1.- Para registrar dron")
-        print("2.- Para mostrar drones")
-        print("3.- Eliminar mision")
-        print("4.- Para registrar misiones")
-        print("5.- Mostrar misiones")
-        print("6.- Eliminar mision")
+    while True:
+        print("1.- Dones")
+        print("2.- Misiones")
+        print("3.- Zonas")
+        print("4.- Rutas")
+        print("5.- Ordenar misiones por prioridad (Burbuja)")
+        print("6.- Ordenar drones por batería (Inserción)")
+        print("7.- Ordenar drones por velocidad (MergeSort)")
+        print("8.- Ordenar misiones por distancia (QuickSort)")
+        print("9.- Búsqueda lineal de dron")
+        print("10.- Búsqueda lineal de misión")
+        print("11.- Búsqueda binaria de dron por código")
+        print("0.- Salir")
         opcion = int(input("Ingrese la opcion: "))
         if opcion == 1:
-            registrar_dron()
-        elif opcion ==2:
-            mostrar_drones()
-        elif opcion ==3:
-            eliminar_dron()
-        elif opcion ==4:
-            registrar_misiones()
+            print("----Menu----")
+            print("1.- Registrar dron")
+            print("2.- Mostrar dron")
+            print("3.- Eliminar dron")
+            opciond = int(input("Escoja la opcion para el dron:"))
+            if opciond == 1:
+                registrar_dron()
+            elif opciond == 2:
+                mostrar_drones()
+            elif opciond == 3:
+                eliminar_dron()
+        elif opcion == 2:
+            print("----Menu----")
+            print("1.- Registrar mision")
+            print("2.- Mostrar mision")
+            print("3.- Eliminar mision")
+            opciond = int(input("Escoja la opcion para la mision:"))
+            if opciond == 1:
+                registrar_misiones()
+            elif opciond == 2:
+                mostrar_misiones()
+            elif opciond == 3:
+                eliminar_mision()
+        elif opcion == 3:
+            print("----Menu----")
+            print("1.- Registrar Zona")
+            print("2.- Mostrar Zona")
+            print("3.- Eliminar Zona")
+            opciond = int(input("Escoja la opcion para la Zona:"))
+            if opciond == 1:
+                registrar_zona()
+            elif opciond == 2:
+                mostrar_zonas()
+            elif opciond == 3:
+                eliminar_zona()
+        elif opcion == 4:
+            print("----Menu----")
+            print("1.- Registrar Ruta")
+            print("2.- Mostrar Ruta")
+            print("3.- Eliminar Ruta")
+            opciond = int(input("Escoja la opcion para la Ruta:"))
+            if opciond == 1:
+                registrar_ruta()
+            elif opciond == 2:
+                mostrar_rutas()
+            elif opciond == 3:
+                eliminar_ruta()
         elif opcion == 5:
-            mostrar_misiones()
+            burbuja_prioridad()
         elif opcion == 6:
-            eliminar_mision()
+            insercion_bateria()
+        elif opcion == 7:
+            mergesort_velocidad(drones)
+        elif opcion == 8:
+            quicksort_distancia(misiones, 0, len(misiones) - 1)
+            print("Resultado")
+            mostrar_codigos(misiones, "Codigo")
+        elif opcion == 9:
+            busqueda_lineal_dron()
+        elif opcion == 10:
+            busqueda_lineal_mision()
+        elif opcion == 11:
+            busqueda_binaria_dron()
+        elif opcion == 0:
+            break
 
 if __name__ == '__main__':
     menuchi()
+
+
+class nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+
+class cola:
+    def __init__(self):
+        self.cabeza = None
+        self.cola = None
+    def guardar(self, dato):
+        nuevo=nodo(dato)
+        if not self.cola:
+            self.cabeza = nuevo
+            self.cola = nuevo
+        else:
+            self.cola.siguiente = nuevo
+            self.cola = nuevo 
+    def sacar(self):
+        if self.cabeza == None:
+            print("No hay elementos en la lista de espera")
+        else:
+            dato = self.cabeza.dato
+            self.cabeza = self.cabeza.siguiente
+            if self.cabeza == None:
+                self.cola = None
+            print(f"Se cambio el estado de {dato} a Atendida")
+    def mostrar(self):
+        if self.cabeza == None:
+            print("No hay elementos en espera")
+        else:
+            lista_espera=[]
+            actual = self.cabeza
+            while actual is not None:
+                lista_espera.append(str(actual.dato))
+                actual = actual.siguiente
+            print(f"Elementos en espera: {lista_espera}")
